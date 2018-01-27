d4ccdef396088885be2307f74b434a04100c6a2d
Jaros Olgierd <OlgierdJaros@gmail.com>
2018-01-27 12:01:57 +0100

    DoubleHeadCow

---
+++ Assets/Scripts/DoubleHeadCow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleHeadCow : MonoBehaviour {
+
+    public GameObject Wave;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject waveUp = (GameObject)Instantiate(Wave, transform.position + new Vector3(1.0f, 0.5f, 0.0f), transform.rotation);
+        GameObject waveDown = (GameObject)Instantiate(Wave, transform.position + new Vector3(1.0f, -0.5f, 0.0f), transform.rotation);
+        float length = 30.0f;
+        Vector2 velocityUp = new Vector2(1.0f, 1.0f) * length;
+        Vector2 velocityDown = new Vector2(1.0f, -1.0f) * length;
+
+        Rigidbody2D rbUp = waveUp.GetComponent<Rigidbody2D>();
+        Rigidbody2D rbDown = waveDown.GetComponent<Rigidbody2D>();
+        rbUp.velocity = velocityUp / 2;
+        rbDown.velocity = velocityDown / 2;
+        Destroy(collision.gameObject);
+    }
+}
