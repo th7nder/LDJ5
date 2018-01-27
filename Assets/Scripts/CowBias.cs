7783a090e7041307adb82286c0f24a6d5cb7c6ba
Jaros Olgierd <OlgierdJaros@gmail.com>
2018-01-27 16:51:45 +0100

    Niespodzianka dla Junde

---
+++ Assets/Scripts/CowBias.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Variables.Definitions;
+
+public class CowBias : MonoBehaviour {
+
+    public GameObject Wave;
+    public FloatVariable ProjectileSpeed;
+    public string FirstWord;
+    Vector2 direction;
+    public enum Direction { left, right, up, down };
+    public Direction selectedDirection;
+
+    private TextMesh text;
+
+    private void Start()
+    {
+        text = GetComponentInChildren<TextMesh>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (selectedDirection)
+        {
+            case Direction.left:
+                SendLeftUp();
+                break;
+
+            case Direction.right:
+                SendRightDown();
+                break;
+
+            case Direction.up:
+                SendRightUp();
+                break;
+
+            case Direction.down:
+                SendLeftDown();
+                break;
+        }
+        Destroy(collision.gameObject);
+        text.text = FirstWord;
+    }
+
+    void SendLeftUp()
+    {
+        GameObject wave = (GameObject)Instantiate(Wave, transform.position + new Vector3(-1.0f, 1.0f, 0.0f), transform.rotation);
+        Vector2 velocity = new Vector2(-1.0f, 1.0f) * ProjectileSpeed.Value;
+
+        Rigidbody2D rb = wave.GetComponent<Rigidbody2D>();
+        rb.velocity = velocity / 2;
+    }
+
+    void SendRightUp()
+    {
+        GameObject wave = (GameObject)Instantiate(Wave, transform.position + new Vector3(1.0f, 1.0f, 0.0f), transform.rotation);
+        Vector2 velocity = new Vector2(1.0f, 1.0f) * ProjectileSpeed.Value;
+
+        Rigidbody2D rb = wave.GetComponent<Rigidbody2D>();
+        rb.velocity = velocity / 2;
+    }
+
+    void SendLeftDown()
+    {
+        GameObject wave = (GameObject)Instantiate(Wave, transform.position + new Vector3(-1.0f, -1.0f, 0.0f), transform.rotation);
+        Vector2 velocity = new Vector2(-1.0f, -1.0f) * ProjectileSpeed.Value;
+
+        Rigidbody2D rb = wave.GetComponent<Rigidbody2D>();
+        rb.velocity = velocity / 2;
+    }
+
+    void SendRightDown()
+    {
+        GameObject wave = (GameObject)Instantiate(Wave, transform.position + new Vector3(1.0f, -1.0f, 0.0f), transform.rotation);
+        Vector2 velocity = new Vector2(1.0f, -1.0f) * ProjectileSpeed.Value;
+
+        Rigidbody2D rb = wave.GetComponent<Rigidbody2D>();
+        rb.velocity = velocity / 2;
+    }
+}
