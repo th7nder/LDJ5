ee9e5fdf1b4e038450d91fac0ef4e29e0013084c
Jaros Olgierd <OlgierdJaros@gmail.com>
2018-01-27 11:42:45 +0100

    Cow

---
+++ Assets/Scripts/Cow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : MonoBehaviour
+{
+    public GameObject Wave;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject wave = (GameObject)Instantiate(Wave, transform.position + new Vector3(1.0f, 0.0f, 0.0f), transform.rotation);
+        float length = 30.0f;
+        Vector2 velocity = Vector2.right * length;
+
+        Rigidbody2D rb = wave.GetComponent<Rigidbody2D>();
+        rb.velocity = velocity / 2;
+    }
+}
