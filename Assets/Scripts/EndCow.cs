97079f48e4122a01c0b58d7038f55b4233b051dc
Jaros Olgierd <OlgierdJaros@gmail.com>
2018-01-28 11:29:40 +0100

    End Cow
    Jeszcze nic nie robi xD

---
+++ Assets/Scripts/EndCow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCow : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Wave")) return;
+        Destroy(collision.gameObject);
+        EndLevel();
+    }
+
+    void EndLevel()
+    {
+
+    }
+}
