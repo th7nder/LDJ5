e96ca0391734034271794c78dd66dfaed569531f
th7nder <th7nderborn@gmail.com>
2018-01-28 06:32:44 +0100

    Fixed some bugs related to sending waves, more code cleanup

---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Wave"))
+        {
+            Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+        }
+
+    }
+}
