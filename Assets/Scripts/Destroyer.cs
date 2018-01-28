fecc4e30a866e080248845db4953f9f3843ad8b1
th7nder <th7nderborn@gmail.com>
2018-01-28 11:21:24 +0100

    Added DeathEvent trigger on Destruction prefabs

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Destroyer : MonoBehaviour {
 
-
+    public UnityEvent DeathEvent;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Wave"))
         {
             Destroy(collision.gameObject);
+            DeathEvent.Invoke();
+
         }
 
     }
