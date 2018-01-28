62ca3ad1fcae13efa3cb6948c966ed154f2ad6c1
th7nder <th7nderborn@gmail.com>
2018-01-28 11:13:27 +0100

    Started changing sending wave concept

--- Assets/Scripts/StartingCow.cs
+++ Assets/Scripts/StartingCow.cs
@@ -14,6 +14,10 @@ public class StartingCow : MonoBehaviour
     // Update is called once per frame
 
 
+
+    bool _isTransmitting = false;
+    int _transmitCounter;
+
     void Start()
     {
         switch (selectedDirection)
@@ -34,12 +38,13 @@ public class StartingCow : MonoBehaviour
                 direction = Vector2.down;
                 break;
         }
-        StartCoroutine("SpawnWave");
+
     }
 
     IEnumerator SpawnWave()
     {
-        while(true)
+        _transmitCounter = 15;
+        while(_transmitCounter > 0)
         {
             GameObject wave = (GameObject)Instantiate(Wave, transform.position + new Vector3(direction.x, direction.y, 0.0f), transform.rotation);
             Vector2 velocity = direction * ProjectileSpeed.Value;
@@ -47,7 +52,23 @@ public class StartingCow : MonoBehaviour
             Rigidbody2D rb = wave.GetComponent<Rigidbody2D>();
             rb.velocity = velocity;
 
+            _transmitCounter--;
             yield return new WaitForSeconds(0.5f);
         }
+
+        _isTransmitting = false;
+
+        yield return null;
+    }
+
+
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !_isTransmitting)
+        {
+            _isTransmitting = true;
+            StartCoroutine("SpawnWave");
+        }
+
     }
 }
