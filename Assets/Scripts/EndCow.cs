525ab96e792f709f76a5796fa7d1723d45e630aa
Jaros Olgierd <OlgierdJaros@gmail.com>
2018-01-28 12:15:26 +0100

    Input on end

--- Assets/Scripts/EndCow.cs
+++ Assets/Scripts/EndCow.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndCow : MonoBehaviour {
 
+    public string EndPhrase;
+    public string NextSceneName;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Wave")) return;
@@ -13,6 +17,17 @@ public class EndCow : MonoBehaviour {
 
     void EndLevel()
     {
+        //show endfield
+    }
 
+    public void CompareStrings(InputField input)
+    {
+        if (input.text.ToLower() == EndPhrase.ToLower()) Success();
+    }
+
+    void Success()
+    {
+        Debug.Log("Succes");
+        //SceneManager.LoadScene(NextSceneName);
     }
 }
